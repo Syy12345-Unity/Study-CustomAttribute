fe54db340b8cde80e74e0f08f72f767a1332b71a
Syy <shirakawa.yy@gmail.com>
2018-08-22 22:55:09 +0900

    Add TestDrawer

---
+++ Assets/0_TestAttribute/TestDrawer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(TestAttribute))]
+public class TestDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.LabelField(position, label);
+        position.x += 50;
+        EditorGUI.LabelField(position, "Test");
+        position.x += 50;
+        EditorGUI.LabelField(position, label);
+    }
+}
