857cf67fae4e93faca8d36620e428a025b966f48
Syy <shirakawa.yy@gmail.com>
2018-08-22 23:33:17 +0900

    Add Range2Attribute

---
+++ Assets/3_Range2Attribute/Range2Attribute.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Range2Attribute : PropertyAttribute
+{
+    public int Min, Max;
+    public Range2Attribute(int min, int max)
+    {
+        Min = min;
+        Max = max;
+    }
+}
---
+++ Assets/3_Range2Attribute/Range2Drawer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(Range2Attribute))]
+public class Range2Drawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if(property.propertyType == SerializedPropertyType.Integer)
+        {
+            var cast = (Range2Attribute)attribute;
+            //property.intValue = EditorGUI.IntSlider(position, property.intValue, cast.Min, cast.Max);
+            EditorGUI.IntSlider(position, property, cast.Min, cast.Max, label);
+        }
+        
+    }
+}
