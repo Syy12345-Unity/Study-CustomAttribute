4812428bad3edf9faf80cf470004c16c6ccf908f
Syy <shirakawa.yy@gmail.com>
2018-08-24 23:52:01 +0900

    Add DisableAttribute

---
+++ Assets/4_DisableAttribute/DisableAttributeSample.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableAttributeSample : MonoBehaviour {
+
+    [Disable]
+    public int hoge;
+}
---
+++ Assets/4_DisableAttribute/DisableDrawer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(DisableAttribute))]
+public class DisableDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        using(new EditorGUI.DisabledScope(true))
+        {
+            EditorGUI.PropertyField(position, property, label);
+        }
+    }
+}
